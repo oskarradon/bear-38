da989b5be34eaa76cfef678ec23f37c585b7edf4
Oskar <oskarradonkimball@gmail.com>
2021-04-04 21:23:42 -0700

    Create home.js

---
+++ assets/js/home.js
@@ -0,0 +1,8 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   hide('.paypal-button-number-1');
+//   hide('.paypal-powered-by');
+// });
+
+// function hide(el) {
+//   document.querySelector(el).style.display = 'none';
+// };
