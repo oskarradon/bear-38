4065d81808f7dc00d927170f8009d6331ad88dec
Oskar <oskarradonkimball@gmail.com>
2021-04-04 21:23:52 -0700

    hiding not working

--- assets/js/home.js
+++
@@ -1,8 +0,0 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   hide('.paypal-button-number-1');
-//   hide('.paypal-powered-by');
-// });
-
-// function hide(el) {
-//   document.querySelector(el).style.display = 'none';
-// };
