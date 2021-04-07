b063cba8b81c2038b71ca6e602f4cb0e22f56d2b
Oskar <oskarradonkimball@gmail.com>
2021-04-07 09:48:34 -0700

    Update home.js

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,17 +1,14 @@
 function checkScrollbar() {
   let hasVScroll = document.body.scrollHeight > document.body.clientHeight
-
   // Get the computed style of the body element
   let cStyle =
     document.body.currentStyle || window.getComputedStyle(document.body, '')
-
   // Check the overflow and overflowY properties for "auto" and "visible" values
   hasVScroll =
     cStyle.overflow == 'visible' ||
     cStyle.overflowY == 'visible' ||
     (hasVScroll && cStyle.overflow == 'auto') ||
     (hasVScroll && cStyle.overflowY == 'auto')
-
   return hasVScroll
 }
 
@@ -22,34 +19,16 @@ function getScrollbarWidth() {
   outer.style.overflow = 'scroll' // forcing scrollbar to appear
   outer.style.msOverflowStyle = 'scrollbar' // needed for WinJS apps
   document.body.appendChild(outer)
-
   // Creating inner element and placing it in the container
   const inner = document.createElement('div')
   outer.appendChild(inner)
-
   // Calculating difference between container's full width and the child width
   const scrollbarWidth = outer.offsetWidth - inner.offsetWidth
-
   // Removing temporary elements from the DOM
   outer.parentNode.removeChild(outer)
 
   return scrollbarWidth
 }
-
-console.log(checkScrollbar(), getScrollbarWidth())
-
-document.addEventListener(
-  'DOMContentLoaded',
-  function () {
-    if (checkScrollbar()) {
-      let sb = getScrollbarWidth()
-      // document.querySelector('body').style.marginRight = sb + 'px'
-      document.querySelector('#btn-order').style.marginRight = sb + 35 + 'px'
-      console.log(document.querySelector('body').style.marginRight)
-    }
-  },
-  false
-)
 
 function getScreenWidth() {
   let width =
@@ -58,3 +37,25 @@ function getScreenWidth() {
     document.body.clientWidth
   return width
 }
+
+function offsetRight(el) {
+  let sb = getScrollbarWidth()
+  el.style.marginRight = sb + 35 + 'px'
+  console.log(checkScrollbar(), getScrollbarWidth())
+  console.log(document.querySelector('#btn-order').style.marginRight)
+}
+
+document.addEventListener(
+  'DOMContentLoaded',
+  () => {
+    if (checkScrollbar()) {
+      let button = document.querySelector('#btn-order')
+      offsetRight(button)
+    }
+  },
+  false
+)
+
+window.addEventListener('resize', () => {
+  console.log(getScreenWidth())
+})
